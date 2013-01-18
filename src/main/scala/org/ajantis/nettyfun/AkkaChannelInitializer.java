d93bdb7f11759c5337aa5e8c300070db946fb869
Dmitry Ivanov <id.ajantis@gmail.com>
2013-01-19 02:52:40 +0400

    [Fixed] fix response messages encoding

--- src/main/scala/org/ajantis/nettyfun/AkkaChannelInitializer.java
+++ src/main/scala/org/ajantis/nettyfun/AkkaChannelInitializer.java
@@ -6,7 +6,7 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpContentCompressor;
 import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
 import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
 
@@ -20,13 +20,13 @@ public class AkkaChannelInitializer extends ChannelInitializer<SocketChannel>{
     @Override
     public void initChannel(SocketChannel ch) {
         ChannelPipeline pipeline = ch.pipeline();
-        pipeline.addLast("timeout", new IdleStateHandler(110, 110, 110){
+        pipeline.addLast("timeout", new IdleStateHandler(0, 110, 0){
             public void channelIdle(ChannelHandlerContext ctx, IdleStateEvent edx){
                 ctx.channel().close();
             }
         });
         pipeline.addLast("decoder", new HttpRequestDecoder());
-        pipeline.addLast("encoder", new HttpRequestEncoder());
+        pipeline.addLast("encoder", new HttpResponseEncoder());
         pipeline.addLast("deflater", new HttpContentCompressor());
         pipeline.addLast("handler", new AkkaServerHandler());
     }
